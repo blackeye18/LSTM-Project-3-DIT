40c4394341ec0a442757ab1b1eb7c2572c600802
blackeye18 <92744692+blackeye18@users.noreply.github.com>
2022-01-19 17:30:04 +0200

    Add files via upload

---
+++ G_Reduce/reduce.py
@@ -0,0 +1,105 @@
+import math
+import matplotlib.pyplot as plt
+import keras
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dropout
+from keras.layers import *
+from keras.layers import Input, Dense, Conv1D, MaxPooling1D, UpSampling1D, BatchNormalization, LSTM, RepeatVector
+from keras.models import Model
+from keras.models import model_from_json
+from keras import regularizers
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from keras.callbacks import EarlyStopping
+from sklearn.preprocessing import StandardScaler
+import csv
+import sys
+#from google.colab import drive
+
+#drive.mount('/content/drive')
+#path = "/content/drive/My Drive/project_3/"
+
+def create_dataset(data,sc,windows=10,):
+  size=data.shape[0]
+  windows_lst,resultX_lst,resultY_lst=[],[],[]
+  for i in range(0,size):
+    v=data[i]
+    windows_lst.append(v)
+    if len(windows_lst) == windows:
+      training_set_scaled = sc.fit_transform(windows_lst)
+      resultX_lst.append(training_set_scaled)
+      resultY_lst.append(windows_lst)
+      windows_lst=[]
+  return np.array(resultX_lst),np.array(resultY_lst)
+
+def print_encoded(df,encoder_model,output_name):
+  dataset=df.iloc[:,:].values
+  iteration=0
+  f = open(path+output_name, 'w')
+  writer = csv.writer(f,delimiter='\t')
+
+  for arr in dataset:
+    """
+    if iteration > 5:
+      break
+    """
+    name=arr[0]
+    data=arr[1:]
+    encoding_dim = 3
+    ##########alla3a ta windows se 10
+    windows=10#encoding_dim*4
+
+    data=data.reshape(-1,1)
+    sc = MinMaxScaler(feature_range = (0, 1))
+    dataX,dataY=create_dataset(data,sc,windows)
+
+    encoded_data=encoder_model.predict(dataX)
+
+    row=[]
+    row.append(name)
+    for x in encoded_data:
+      for y in x:
+        #print("y:",str(y).lstrip('[').rstrip(']'))
+        row.append(str(y).lstrip('[').rstrip(']'))
+    #for itemn in row:
+      #writer.writerow(itemn)
+    #new_lst = (','.join(row))
+    #print(row)
+    writer.writerow(row)
+    iteration+=1
+
+  print("length of row after",len(row))
+  print("number of rows after",iteration)
+  f.close()
+
+#$python reduce.py –d <dataset> -q <queryset> -od <output_dataset_file> -oq <output_query_file>
+
+path=""
+#ta onomata dinonte aptin grammi entolwn
+dataset="nasdaq2007_17.csv"#onoma dataset prin to encoding
+queryset="nasdaq2007_17.csv"#onoma queryset prin to encoding
+output_dataset_file="output_dataset_file.csv"#onoma dataset meta to encoding
+output_query_file="output_query_file.csv"#onoma queryset meta to encoding
+
+if len(sys.argv) != 9:
+    print("Wrong no of arguments!")
+    exit(1)
+
+dataset=sys.argv[2]
+queryset=sys.argv[4]
+output_dataset_file=sys.argv[6]
+output_query_file=sys.argv[8]
+
+data_df=pd.read_csv(path+dataset,'\t',header=None)
+query_df=pd.read_csv(path+queryset,'\t',header=None)
+print("shape before",data_df.shape)
+########kanw load ton encoder
+encoder_model=keras.models.load_model(path+"encoder1.h5")
+
+print_encoded(data_df,encoder_model,output_dataset_file)
+print_encoded(query_df,encoder_model,output_query_file)
+
---
+++ G_Reduce/train_reduce.py
@@ -0,0 +1,182 @@
+import math
+import matplotlib.pyplot as plt
+import keras
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dropout
+from keras.layers import *
+from keras.layers import Input, Dense, Conv1D, MaxPooling1D, UpSampling1D, BatchNormalization, LSTM, RepeatVector
+from keras.models import Model
+from keras.models import model_from_json
+from keras import regularizers
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from keras.callbacks import EarlyStopping
+from sklearn.preprocessing import StandardScaler
+import random
+import sys
+
+#from google.colab import drive
+
+
+def create_dataset(data, sc, windows=10, ):
+    size = data.shape[0]
+    windows_lst, resultX_lst, resultY_lst = [], [], []
+    for i in range(0, size):
+        v = data[i]
+        windows_lst.append(v)
+        if len(windows_lst) == windows:
+            training_set_scaled = sc.fit_transform(windows_lst)
+            resultX_lst.append(training_set_scaled)
+            resultY_lst.append(windows_lst)
+            windows_lst = []
+    return np.array(resultX_lst), np.array(resultY_lst)
+
+
+def plot_history(history):
+    plt.figure(figsize=(15, 5))
+    ax = plt.subplot(1, 2, 1)
+    plt.plot(history.history["loss"])
+    plt.title("Train loss")
+    ax = plt.subplot(1, 2, 2)
+    plt.plot(history.history["val_loss"])
+    plt.title("Test loss")
+
+
+def plot_examples(stock_input, stock_decoded):
+    n = stock_input.shape[1]
+    plt.figure(figsize=(20, 4))
+    test_samples = train_size
+    for i, idx in enumerate(list(np.arange(0, stock_input.shape[0], n))):
+        # print("i=",i,"  idx=",idx)
+
+        # display original
+        ax = plt.subplot(2, n, i + 1)
+        if i == 0:
+            ax.set_ylabel("Input", fontweight=600)
+        else:
+            ax.get_yaxis().set_visible(False)
+        plt.plot(stock_input[idx])
+        ax.get_xaxis().set_visible(False)
+
+        # display reconstruction
+        ax = plt.subplot(2, n, i + 1 + n)
+        if i == 0:
+            ax.set_ylabel("Output", fontweight=600)
+        else:
+            ax.get_yaxis().set_visible(False)
+        plt.plot(stock_decoded[idx])
+        ax.get_xaxis().set_visible(False)
+    plt.show()
+
+
+#drive.mount('/content/drive')
+# input_path = "/content/drive/My Drive/project_3/input.csv"
+#path = "/content/drive/My Drive/project_3/"
+# file_name="nasd_input.csv"
+
+path=""
+file_name = "nasdaq2007_17.csv"
+N = 359
+if len(sys.argv) < 5:
+    print("Wrong no of arguments!")
+    exit(1)
+
+N=int(sys.argv[4])
+file_name=sys.argv[2]
+
+encoder_name="encoder_1.h5"
+if len(sys.argv) > 5:
+    encoder_name=sys.argv[5]
+
+input_path = path + file_name
+
+df = pd.read_csv(input_path, '\t', header=None)
+print("Number of rows and columns:", df.shape)
+# print(df.head(5))
+
+if N>df.shape[0]:
+    N=df.shape[0]
+    print("Wrong n(n>size of dataset), new n=",N)
+
+dataset = df.iloc[:, 1:].values
+#print(dataset.shape)
+
+train_size = int(0.8 * dataset.shape[1])
+test_size = (dataset.shape[1] - train_size)
+
+
+sequence = list(range(0, df.shape[0]))
+random.shuffle(sequence)
+
+
+N_train = N  # gia na kanei train mono me to dataset pu tha kanei predict
+# N_train=df.shape[0]#gia na kanei train me olo to dataset
+test_arr = []
+
+train_count=0
+
+#debug_flag=0 #gia na emfanizei grafikes parastaseis gia debug, dhladh gia na vlepoume an oi times pou allazoume exoyn kalytera apotelesmata h oxi.
+debug_flag=1#gia na mhn emfanizei grafikes parastaseis
+
+
+for iteration in range(0, N_train):
+    train_count=train_count+1
+    arr = dataset[sequence[iteration]]
+
+    encoding_dim = 3
+    windows = 10  # encoding_dim*4
+
+    train_df = arr[:train_size].reshape(-1, 1)
+    sc = MinMaxScaler(feature_range=(0, 1))
+    trainX, trainY = create_dataset(train_df, sc, windows)
+    print(trainX.shape)
+    print(trainY.shape)
+
+    test_df = arr[train_size:].reshape(-1, 1)
+    sc = MinMaxScaler(feature_range=(0, 1))
+    testX, testY = create_dataset(test_df, sc, windows)
+
+    if iteration == 0:
+        input_window = Input(shape=(windows, 1))
+        x = Conv1D(16, 3, activation="relu", padding="same")(input_window)  # 10 dims
+        # x = BatchNormalization()(x)
+        x = MaxPooling1D(2, padding="same")(x)  # 5 dims
+        x = Conv1D(1, 3, activation="relu", padding="same")(x)  # 5 dims
+        # x = BatchNormalization()(x)
+        encoded = MaxPooling1D(2, padding="same")(x)  # 3 dims
+
+        encoder = Model(input_window, encoded)
+
+        #################allagi kanw compile twn encoder
+        encoder.compile(optimizer='adam', loss='binary_crossentropy')
+        # 3 dimensions in the encoded layer
+        x = Conv1D(1, 3, activation="relu", padding="same")(encoded)  # 3 dims
+        # x = BatchNormalization()(x)
+        x = UpSampling1D(2)(x)  # 6 dims
+        x = Conv1D(16, 2, activation='relu')(x)  # 5 dims
+        # x = BatchNormalization()(x)
+        x = UpSampling1D(2)(x)  # 10 dims
+        decoded = Conv1D(1, 3, activation='sigmoid', padding='same')(x)  # 10 dims
+        autoencoder = Model(input_window, decoded)
+        autoencoder.summary()
+
+        autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+
+    history = autoencoder.fit(trainX, trainX, epochs=100, batch_size=1024, shuffle=True, validation_data=(testX, testX))
+
+    # encoded_values = encoder.predict(testX)# entoli gia predictions encoded
+    # print("encoded_values ",encoded_values.shape)
+    testX_predicted = autoencoder.predict(testX)
+
+    if debug_flag == 0:
+        plot_history(history)
+        print("testX shape", testX.shape)
+        plot_examples(testX, testX_predicted)
+#############allagi kanw save ton encoder
+
+encoder.save(path + encoder_name )
+print("Finished :) Trained with: ",train_count)
